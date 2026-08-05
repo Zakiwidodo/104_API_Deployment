fb94a63b2882aef4acf7aa5b66a74075296e1a1c
ZakiWidodo <muhzakiwidodo@gmail.com>
2026-08-05 10:09:46 +0700

    commit penulis lagi

--- models/penulis.js
+++ models/penulis.js
@@ -22,6 +22,13 @@ module.exports = (sequelize, DataTypes) => {
       tableName: "penulis",
       timestamps: true
     });
+    Penulis.associate = (models) => {
+        Penulis.hasMany(models.Komik, {
+            foreignKey: "penulis_id",
+            as: "komik"
+        });
+    };
+
   
     return Penulis;
   };
