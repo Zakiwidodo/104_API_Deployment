1eeba8a8a2183a9b862e6360f6fd85552579d417
ZakiWidodo <muhzakiwidodo@gmail.com>
2026-08-19 13:56:24 +0700

    upadte

--- routers/api.js
+++ routers/api.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-// Route POST untuk testing /api/users
+// Handler GET /api/users
+router.get("/users", (req, res) => {
+  res.status(200).json({
+    status: true,
+    message: "Berhasil mengambil data users!",
+    data: []
+  });
+});
+
+// Handler POST /api/users
 router.post("/users", (req, res) => {
   const { username, email, password } = req.body;
-  
   res.status(201).json({
     status: true,
     message: "User berhasil dibuat!",
